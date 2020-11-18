d50514e0ee340e96e9594b16e1851a75203e33fe
eulis01 <eulis01@gmail.com>
2020-11-18 14:15:06 -0500

    updated with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201118190038) do
+ActiveRecord::Schema.define(version: 20201118190907) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
